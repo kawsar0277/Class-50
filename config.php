b039c4d8e81e2dc86757d372061b46a566ab37c8
kawsar0277 <kawsar1252005@gmail.com>
2025-08-12 21:43:09 +0600

    class-50 is added in repository

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php declare(strict_types=1); ?>
+
+<?php
+$connection = new mysqli('localhost', 'root', '', 'school_mgt_1');
+
+// if ($connection == true) {
+//     echo "connection successfull";
+// } else {
+//     echo "unseccessfull";
+// }
+?>
---
+++ index.php
@@ -0,0 +1,120 @@
+<?php declare(strict_types=1); ?>
+
+<?php
+
+include 'config.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Database</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
+</head>
+
+<body>
+    <div>
+        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="index.php">Navbar</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                        <li class="nav-item">
+                            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="index.php">Link</a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown"
+                                aria-expanded="false">
+                                Dropdown
+                            </a>
+                            <ul class="dropdown-menu">
+                                <li><a class="dropdown-item" href="index.php">Action</a></li>
+                                <li><a class="dropdown-item" href="#">Another action</a></li>
+                                <li>
+                                    <hr class="dropdown-divider">
+                                </li>
+                                <li><a class="dropdown-item" href="index.php">Something else here</a></li>
+                            </ul>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                        </li>
+                    </ul>
+
+                </div>
+            </div>
+        </nav>
+    </div>
+
+    <div class="container mt-5">
+        <table class="table">
+            <thead>
+                <tr>
+                    <th scope="col">SL.</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Class</th>
+                    <th scope="col">Roll</th>
+                    <th scope="col">Phone</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Address</th>
+                    <th scope="col">Blood_Group</th>
+                </tr>
+            </thead>
+            <tbody>
+
+                <?php
+                $query = 'SELECT * FROM student';
+                $students = mysqli_query($connection, $query);
+
+                $i = 1;
+
+                while ($row = mysqli_fetch_assoc($students)) {
+
+                    $id = $row['id'];
+                    $name = $row['name'];
+                    $class = $row['class'];
+                    $roll = $row['roll'];
+                    $phone = $row['phone'];
+                    $email = $row['email'];
+                    $address = $row['address'];
+                    $blood = $row['blood_group'];
+
+                    echo '<tr>
+                    <th scope="row">' . $i . '</th>
+                    <td>' . $name . '</td>
+                    <td>' . $class . '</td>
+                    <td>' . $roll . '</td>
+                    <td>' . $phone . '</td>
+                    <td>' . $email . '</td>
+                    <td>' . $address . '</td>
+                    <td>' . $blood . '</td>
+                    </tr>';
+
+                    $i++;
+                }
+
+                ?>
+            </tbody>
+        </table>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ndDqU0Gzau9qJ1lfW4pNLlhNTkCfHzAVBReH9diLvGRem5+R9g2FzA8ZGN954O5Q"
+        crossorigin="anonymous"></script>
+
+</body>
+
+</html>
